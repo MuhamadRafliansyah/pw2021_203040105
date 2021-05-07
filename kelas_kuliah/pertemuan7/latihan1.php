edba0002fe4458372d0461f552236502456093e1
muhamadrafliansyah <rafliansyah2001@gmail.com>
2021-05-08 01:13:41 +0700

    pertemuan6

--- kelas_kuliah/pertemuan7/latihan1.php
+++ kelas_kuliah/pertemuan7/latihan1.php
@@ -1,6 +1,73 @@
 <?php
-// Variable Scope / lingkup variable
-$x = 10;
-echo $x;
+// Variable Scope / lingkup variabel
+// $x = 10;
+// function tampilX() {
+//     global $x;
+//     echo $x;
+// }
+// tampilX();
 
+// SUPERGLOBALS
+// variable global milik PHP
+// merupakan Array Associative
+
+// $_GET
+$mahasiswa = [
+    
+    [
+        "nama" => "Hamzah Hadi P.",
+        "nrp" => "203040087",
+        "email" => "hamzahhadi@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "1.jpg"
+    ],
+    [
+        "nama" => "Ray Gineung P.",
+        "nrp" => "203040099",
+        "email" => "raygineng@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "2.jpg"
+    ],
+    [
+        "nama" => "M.Rafliansyah",
+        "nrp" => "203040105",
+        "email" => "raflimuhamad@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "3.jpg"
+    ],
+    [
+        "nama" => "Paiz Abdul J.",
+        "nrp" => "203040108",
+        "email" => "paizuz@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "4.jpg"
+    ],
+    [
+        "nama" => "Farhan Alfauzi",
+        "nrp" => "203040085",
+        "email" => "farhanalf@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "5.jpg"
+    ],
+];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+    <ul>
+    <?php foreach($mahasiswa as $mhs) : ?>
+        <li>
+            <a href="latihan2.php?nama=<?= $mhs["nama"]; ?>&nrp=<?= $mhs["nrp"]; ?>&email=<?= $mhs["email"]; ?>&jurusan=<?= $mhs["jurusan"]; ?>&gambar=<?= $mhs["gambar"]; ?>"><?= $mhs["nama"]; ?></a>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+</body>
+</html>
